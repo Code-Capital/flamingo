9de558ed42b0b9207c20612db780d5bb9402266b
Muhammad Arslan <mughalarslan996@gmail.com>
2024-08-28 15:21:45 +0500

    code refactor

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -83,10 +83,10 @@
         </div>
     </main>
     <script src="{{ asset('js/sweetalert.js') }}"></script>
-    <script src=" {{ asset('js/jquery-3.6.3.js') }} "></script>
-    <script src=" {{ asset('js/popper.min.js') }} "></script>
-    <script src=" {{ asset('js/bootstrap.min.js') }} "></script>
-    <script src=" {{ asset('js/select2.min.js') }} "></script>
+    <script src="{{ asset('js/jquery-3.6.3.js') }} "></script>
+    <script src="{{ asset('js/popper.min.js') }} "></script>
+    <script src="{{ asset('js/bootstrap.min.js') }} "></script>
+    <script src="{{ asset('js/select2.min.js') }} "></script>
     <script src="{{ asset('js/dropzone.js') }}"></script>
     <script src="{{ asset('js/toastr.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
