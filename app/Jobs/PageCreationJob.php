d301733c1488ac2cbd67a5e4a6ae0ddf25888070
Muhammad Arslan <mughalarslan996@gmail.com>
2024-09-04 12:50:31 +0500

    add page creation notification

---
+++ app/Jobs/PageCreationJob.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Events\PageCreationEvent;
+use App\Models\Page;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class PageCreationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $page;
+    public $user;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(Page $page, User $user)
+    {
+        $this->page = $page;
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info('PAGE CREATION JOB -> Stated: ');
+        foreach ($this->user->friends as $friend) {
+            Log::info('CREATING JOB EVENTS->: ' . $this->page->name);
+            broadcast(new PageCreationEvent($this->page, $friend));
+        }
+
+        Log::info('PAGE CREATION JOB -> Completed: ');
+    }
+}
+
---
+++ app/Listeners/PageCreationListener.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\PageCreationEvent;
+use App\Enums\NotificationStatusEnum;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PageCreationListener implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(PageCreationEvent $event): void
+    {
+        // Generate the link to the post
+        $pageLink = route('events.show', $event->page->slug);
+
+        // Create the HTML message
+        $body = limitString($event->page->title, 20);
+        $message = "
+            <div class='notification'>
+                <strong>{$event->user->full_name}</strong> create a new event <a href='{$pageLink}' target='_blank'>{$body}</a>
+            </div>
+        ";
+
+        $event->user->notifications()->create([
+            'type' => NotificationStatusEnum::PAGECREATED->value,
+            'data' => json_encode([
+                'message' => $message,
+                'page_id' => $event->page->id,
+                'user_id' => $event->user->id,
+                'user_name' => $event->user->full_name,
+            ]),
+        ]);
+    }
+}
