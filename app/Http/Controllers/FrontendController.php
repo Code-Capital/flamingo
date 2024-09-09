4cf0c2303c9500be26d1f5cec67909b24eb0149e
Muhammad Arslan <mughalarslan996@gmail.com>
2024-09-09 18:48:04 +0500

    create dynamic plan settings

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Mail\ContactMail;
 use App\Models\Plan;
+use App\Models\Setting;
 use App\Models\TermsAndConditions;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Schema;
 
 class FrontendController extends Controller
 {
@@ -22,6 +24,14 @@ class FrontendController extends Controller
     {
         $plans = Plan::active()->get();
         $isSubscribed = Auth::user() ? Auth::user()->subscribed('default') : false;
+        $settingsArray = Setting::first()->toArray();
+        // Filter the keys that start with 'sub_'
+        $filteredSettings = array_filter($settingsArray, function ($key) {
+            return strpos($key, 'sub_') === 0;
+        }, ARRAY_FILTER_USE_KEY);
+
+        $settings = $filteredSettings;
+        // dd($settings);
 
         return view('pricing', get_defined_vars());
     }
